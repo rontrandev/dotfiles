7c97023af67b311caa5ec138a1c69a9056ce34ec
Ron Tran <ron@rontran.dev>
2025-07-04 20:41:27 +0700

    feat: add treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "css",
+      "elixir",
+      "go",
+      "html",
+      "java",
+      "javascript",
+      "kotlin",
+      "typescript",
+      "yaml",
+      "json",
+      "dockerfile",
+      "svelte",
+    },
+  },
+}
