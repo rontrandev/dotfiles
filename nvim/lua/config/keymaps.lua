091faa168fca7ba5c41dc6c4a6b8ee3c57612c44
Ron Tran <152880468+rontrandev@users.noreply.github.com>
2025-07-04 08:23:24 +0700

    feat: configure tab navigation (#3)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+-- TAB NAVIGATION
+map("n", "gt", "<cmd>bprevious<cr>", { desc = "Go to previous tab page" })
+map("n", "gT", "<cmd>bnext<cr>", { desc = "Go to next tab page" })
