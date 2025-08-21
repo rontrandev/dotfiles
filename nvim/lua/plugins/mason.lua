0c2f0c8d2622593ca87f7043a2cb0c237cb48a05
Ron Tran <ron@rontran.dev>
2025-08-22 06:27:54 +0700

    feat: add emmet ls

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,8 +2,9 @@ return {
   "williamboman/mason.nvim",
   opts = {
     ensure_installed = {
-      "gopls",
       "css-lsp",
+      "emmet-language-server",
+      "gopls",
       "html-lsp",
       "typescript-language-server",
     },
