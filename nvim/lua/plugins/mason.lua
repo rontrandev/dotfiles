1218f3b1a8aad3dd418b89019bac30a159008324
Ron Tran <152880468+rontrandev@users.noreply.github.com>
2025-10-24 20:57:39 +0700

    feat: add lint for ansible (#8)

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,9 @@
 return {
-  "williamboman/mason.nvim",
+  "mason-org/mason.nvim",
   opts = {
     ensure_installed = {
+      "ansible-language-server",
+      "ansible-lint",
       "css-lsp",
       "emmet-language-server",
       "gopls",
