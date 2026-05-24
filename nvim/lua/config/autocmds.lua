83538981a92a7b76c70f21f99ee75635bad27ed8
Ron Tran <ron@rontran.dev>
2026-05-24 20:15:30 +0700

    improve: exclude sensitive files from NeoCodeium

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable AI completions for sensitive files
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { ".env", ".env.*", ".envrc", "*.pem", ".secret*", "auth.json" },
+  callback = function(args)
+    vim.b[args.buf].neocodeium_enabled = false
+  end,
+})
